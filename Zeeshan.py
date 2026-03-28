2bf5fb551cdbef23277bb478e53677efc121cae4
itabishhh <2023475966.zeeshan@ug.sharda.ac.in>
2026-03-28 15:55:42 +0530

    Remove API key from Zeeshan.py

--- Zeeshan.py
+++ Zeeshan.py
@@ -1,7 +1,7 @@
 import os
 
 # Set the OpenAI API key
-os.environ["OPENAI_API_KEY"] = "AIzaSyCd2zRTHJYvB-2nieLi3eGJ7ssrewjdEH4"
+os.environ["OPENAI_API_KEY"] = ""
 
 # Other code below...
 # Placeholder for line 11
